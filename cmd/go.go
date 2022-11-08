a70669c6e08853b96fe6e7ee38e2eee1773eb029
Dar-rius <mohamedtine17@gmail.com>
2022-11-08 18:40:36 +0000

    opening script.bash depending on the os

--- cmd/go.go
+++ cmd/go.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"os/exec"
+	"runtime"
 
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -14,7 +15,7 @@ import (
 var goCmd = &cobra.Command{
 	Use:   "go",
 	Short: "This command will allow you to move to the repository of a project that you have chosen",
-	Long: `This command will allow you to move to the repository of a project that you have chosen`,
+	Long:  `This command will allow you to move to the repository of a project that you have chosen`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if args == nil && args[0] == "" || len(args) > 1 || len(args) < 1 {
 			log.Fatal("Command error")
@@ -29,6 +30,7 @@ var goCmd = &cobra.Command{
 func goPath(project *string) {
 	//the environment variable is stored in a variable in order to create and find the path.json file in the directory where the app is located
 	filEnv := os.Getenv("gproject")
+	sys := runtime.GOOS
 	viper.SetConfigName("path")
 	viper.SetConfigType("json")
 	viper.AddConfigPath(filEnv)
@@ -48,22 +50,40 @@ func goPath(project *string) {
 		panic(err)
 	}
 
-	writeBash(&dir)
+	writeBash(&dir, &sys)
 
-	if err := exec.Command("cmd", "/C", "start", filEnv+`/script.sh`).Run(); err != nil {
-		log.Fatal(err)
+	if sys == "windows" {
+		if err := exec.Command("cmd", "/C", "start", filEnv+`/script.sh`).Run(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if err := exec.Command("bash", "~", filEnv+`script.sh`).Run(); err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
 
 //The writeBash function allows to write scripts in the script.sh file to be executed
-func writeBash(dir *string) {
-	command := "cd " + *dir + "\n bash \n"
-	pathEnv := os.Getenv("gproject")
-	data := []byte(command)
-	err := ioutil.WriteFile(pathEnv+`/script.sh`, data, 0666)
-	if err != nil {
-		log.Fatal(err)
+func writeBash(dir *string, sys *string) {
+	if *sys == "windows" {
+		command := "cd " + *dir + "\n cmd \n"
+		pathEnv := os.Getenv("gproject")
+		data := []byte(command)
+		err := ioutil.WriteFile(pathEnv+`/script.sh`, data, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		command := "cd " + *dir + "\n bash \n"
+		pathEnv := os.Getenv("gproject")
+		data := []byte(command)
+		err := ioutil.WriteFile(pathEnv+`script.sh`, data, 0666)
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
+
 }
 
 func init() {
